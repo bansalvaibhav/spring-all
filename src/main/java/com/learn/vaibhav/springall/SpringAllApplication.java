8a2487536b33377c4431137521edf3383d012219
bansalvaibhav <vaibhav_ban331@yahoo.com>
2018-08-31 18:14:38 +0530

    first commit

---
+++ src/main/java/com/learn/vaibhav/springall/SpringAllApplication.java
@@ -0,0 +1,12 @@
+package com.learn.vaibhav.springall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAllApplication.class, args);
+	}
+}
